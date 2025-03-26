848b7cd1bdeda27e1d30a90ab32c9e89a60a6ce7
Kai Chevannes <chevannes.kai@gmail.com>
2025-03-26 11:41:07 +0000

    feat: exercise 2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcSet="
+        /images/hero-img.avif 1x,
+        /images/hero-img@2x.avif 2x,
+        /images/hero-img@3x.avif 3x,
+        "
+        />
+        <HeroImage src="/images/hero-img.jpg" />
+      </picture>
       <Swoop src="/swoop.svg" />
     </Wrapper>
   );
--- src/components/PhotoGridItem/PhotoGridItem.jsx
+++ src/components/PhotoGridItem/PhotoGridItem.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type="image/avif"
+            srcSet={`
+              ${src.replace(".jpg", ".avif")} 1x,
+              ${src.replace(".jpg", "@2x.avif")} 2x,
+              ${src.replace(".jpg", "@3x.avif")} 3x,
+            `}
+          ></source>
+          <Image alt={alt} src={src} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,6 +38,8 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
